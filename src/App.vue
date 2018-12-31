7b714628fa674c3e376fe7579943b267e7ba9a8c
plety <43912804@qq.com>
2018-12-31 18:29:01 +0800

    2

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <!-- <router-view/> -->
+    123
+  </div>
+</template>
+
+<script>
+// import './router.js'
+export default {
+  name: 'App'
+}
+</script>
+<style>
+*{margin:0;padding:0}
+</style>
